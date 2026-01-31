cb8ba365f20dda1313760e55ede2898ccf1a5c05
loading <loading456rgm@gmail.com>
2026-01-31 23:45:59 +0300

    fix the error in the handler. add a function to check the element.

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 class RandomImage {
-  selectors = {
-    button: 'data-button-element',
-    image: 'data-image-element'
-  }
+    selectors = {
+        button: 'data-button-element',
+        image: 'data-image-element'
+    }
 
-  imageElement = document.querySelector(`[${this.selectors.image}]`)
+    imageElement = document.querySelector(`[${this.selectors.image}]`)
 
-  constructor(api) {
-    this.api = api
-    this.bindEvents()
-  }
+    constructor(api) {
+        this.api = api
+        this.bindEvents()
+    }
 
-  setRandomImage() {
-    fetch(this.api)
-      .then((res) => this.imageElement.src = res.url)
-  }
+    setRandomImage() {
+        fetch(this.api)
+            .then((res) => {
+                this.imageElement.src = res.url
+            })
+    }
 
-  bindEvents() {
-    document.addEventListener('click', (event) => {
-      const { target } = event
-      const isRandomButton = target.hasAttribute(this.selectors.button)
+    checkElement(event) {
+        const { target } = event
+        const isRandomButton = target.hasAttribute(this.selectors.button)
 
-      if (!isRandomButton) {
-        return
-      }
+        if (!isRandomButton) {
+            return
+        }
 
-      this.setRandomImage()
-    })
+        this.setRandomImage()
+    }
 
-    window.addEventListener('load', this.setRandomImage())
-  }
+    bindEvents() {
+        document.addEventListener('click', (event) => this.checkElement(event))
+        window.addEventListener('load', () => this.setRandomImage())
+    }
 }
 
 new RandomImage('https://picsum.photos/700/400?random=1')
